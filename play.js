16bc4e1f5adf65457c922030d7ccbf94a1ad46f3
ftennisco <f.tennisco@hotmail.com>
2023-06-16 14:29:32 -0700

    Created an input file for the snake game and exported the setUpInput code to play.js.

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client");
-
+const { setUpInput } = require("./input");
 
 console.log("Connecting ...");
 const conn = connect();
-
-const setUpInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-
-};
 
 setUpInput();
